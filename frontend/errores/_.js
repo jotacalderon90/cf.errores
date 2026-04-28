80f1f82ec64553431c0eaf2a19cb707adc626c38
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-04-27 23:13:15 -0400

    mejora ui y ux por antigravity

--- frontend/errores/_.js
+++ frontend/errores/_.js
@@ -6,6 +6,11 @@ const errores = function() {
 
 errores.prototype.start = async function(parent){
 	this.parent = parent;
+	this.filterDomain = '';
+	this.sortColumn = 'date';
+	this.sortOrder = -1;
+	this.domains = [];
+	this.displayCollection = [];
 	await this.reload();
 }
 
@@ -21,9 +26,15 @@ errores.prototype.reload = async function(){
 		}
 		
 		this.collection = response.data.map((row)=>{
-			row.date = luxon.DateTime.fromISO(row.date).toFormat('dd-MM-yyyy hh:mm:ss');
+			row.rawDate = row.date;
+			row.date = luxon.DateTime.fromISO(row.date).toFormat('dd-MM-yyyy HH:mm:ss');
 			return row;
 		});
+		
+		const domainsSet = new Set(this.collection.map(row => row.host));
+		this.domains = Array.from(domainsSet).sort();
+
+		this.applyFilters();
 		
 		this.parent.loader.active = false;
 		
@@ -31,6 +42,40 @@ errores.prototype.reload = async function(){
 		alert(error);
 		console.log(error);
 	}
+}
+
+errores.prototype.applyFilters = function() {
+	let result = [...this.collection];
+
+	if (this.filterDomain) {
+		result = result.filter(row => row.host === this.filterDomain);
+	}
+
+	result.sort((a, b) => {
+		let valA = a[this.sortColumn];
+		let valB = b[this.sortColumn];
+
+		if (this.sortColumn === 'date') {
+			valA = a.rawDate;
+			valB = b.rawDate;
+		}
+
+		if (valA < valB) return -1 * this.sortOrder;
+		if (valA > valB) return 1 * this.sortOrder;
+		return 0;
+	});
+
+	this.displayCollection = result;
+}
+
+errores.prototype.toggleSort = function(column) {
+	if (this.sortColumn === column) {
+		this.sortOrder = this.sortOrder * -1;
+	} else {
+		this.sortColumn = column;
+		this.sortOrder = 1;
+	}
+	this.applyFilters();
 }
 
 app.modules.errores = errores;
